5f7e2d1e79793034af5c4cac7b87ef3fcf3d4ebe
deven7818 <deven7818@gmail.com>
2022-04-22 14:23:42 +0530

    Calculating Emp daily wage according to attendance

--- EmployeeWage/EmployeeWage.php
+++ EmployeeWage/EmployeeWage.php
@@ -1,12 +1,16 @@
 <?php
-echo "Welcome to Employee Wage Computation Problem";
+echo "Welcome to Employee Wage Computation Problem\n";
+
 /**
- * UC-1
- * Program for Employee Attendence - Employee Present or not
+ * UC-2
  * Employee wage Computation Program
+ * Calculating daily employee wage
  */
 class EmployeeWage
 {
+    public $WAGE_PER_HR = 20;
+    public $FULL_TIME_WORKING_HRS = 8;
+
     /**
      * function to check employee is present or not
      * using rand() function to generate random number 0 or 1 for Attendance
@@ -15,13 +19,25 @@ class EmployeeWage
     {
         $empCheck = rand(0, 1);
         if ($empCheck == 1) {
-            echo "\nEmployee is present";
+            echo "Employee is Present\n";
+            return $this->FULL_TIME_WORKING_HRS;
         } else {
-            echo "\nEmployee is absent";
+            echo "Employee is Absent\n";
+            return 0;
         }
     }
+    /**
+     * Function to Calculate Daily Wage
+     * calculating the daily wage according to working hours
+     */
+    function dailyWage()
+    {
+        $totalHrs = $this->attendance();
+        $dailyWage = $this->WAGE_PER_HR * $totalHrs;
+        echo "Daily Wage : " . $dailyWage;
+    }
 }
-//creating object of class
+//Creating object
 $empWage = new EmployeeWage();
-$empWage->attendance();
+$empWage->dailyWage();
 ?>
